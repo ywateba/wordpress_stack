d2d90e06f67dd3b30c7d049ece3b2f2146fe4aa5
ywateba <yao.wateba@gmail.com>
2023-09-23 22:03:32 +0200

    deploy updates

--- deploy/security.tf
+++ deploy/security.tf
@@ -2,22 +2,31 @@ provider "aws" {
   region = "us-east-1" # Change to your desired AWS region
 }
 
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "wordpress_bucket" {
   bucket = "wordpress-bucket" # Replace with your desired S3 bucket name
   acl    = "private" # You can adjust the ACL as needed for your security requirements
 }
 
-resource "aws_s3_bucket_object" "private_key" {
-  bucket       = aws_s3_bucket.my_bucket.id
-  key          = "wordpress-keypair"
-  source       = "./wordpress-keypair" # Replace with the path to your private key
+resource "aws_s3_bucket_object" "wordpress_private_key" {
+  bucket       = aws_s3_bucket.wordpress_bucket.id
+  key          = "wordpress/keypair/private.key"
+  source       = ".local/wordpress-keypair" # Replace with the path to your private key
   content_type = "application/octet-stream"
-  etag         = filemd5("./wordpress-keypair") # Optional but recommended
+  etag         = filemd5(".local/wordpress-keypair") # Optional but recommended
 }
 
-resource "aws_key_pair" "my_key_pair" {
+resource "aws_s3_bucket_object" "wordpress_public_key" {
+  bucket       = aws_s3_bucket.wordpress_bucket.id
+  key          = "wordpress/keypair/public.key"
+  source       = ".local/wordpress-keypair.pub" # Replace with the path to your private key
+  content_type = "application/octet-stream"
+  etag         = filemd5(".local/wordpress-keypair.pub") # Optional but recommended
+}
+
+resource "aws_key_pair" "wordpress_key_pair" {
   key_name   = "wordpress-keypair"
-  public_key = file("./wordpress-keypair.pub") # Use the path to your public key
+  public_key = file(".local/wordpress-keypair.pub") # Use the path to your public key
+
 }
 
 
